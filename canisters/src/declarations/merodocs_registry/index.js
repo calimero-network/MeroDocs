25571ed9331d4b47929fe4400b7ccc649a3452f2
Ronit <ronit_c@me.iitr.ac.in>
2025-08-30 16:21:34 +0530

    Refactor llm_chatbot canister: Update dependencies, simplify chat logic, and enhance response formatting

--- canisters/src/declarations/merodocs_registry/index.js
+++ canisters/src/declarations/merodocs_registry/index.js
@@ -1,8 +1,8 @@
 import { Actor, HttpAgent } from "@dfinity/agent";
 
 // Imports and re-exports candid interface
-import { idlFactory } from "./backend.did.js";
-export { idlFactory } from "./backend.did.js";
+import { idlFactory } from "./merodocs_registry.did.js";
+export { idlFactory } from "./merodocs_registry.did.js";
 
 /* CANISTER_ID is replaced by webpack based on node environment
  * Note: canister environment variable will be standardized as
@@ -10,7 +10,7 @@ export { idlFactory } from "./backend.did.js";
  * beginning in dfx 0.15.0
  */
 export const canisterId =
-  process.env.CANISTER_ID_BACKEND;
+  process.env.CANISTER_ID_MERODOCS_REGISTRY;
 
 export const createActor = (canisterId, options = {}) => {
   const agent = options.agent || new HttpAgent({ ...options.agentOptions });
@@ -39,4 +39,4 @@ export const createActor = (canisterId, options = {}) => {
   });
 };
 
-export const backend = canisterId ? createActor(canisterId) : undefined;
+export const merodocs_registry = canisterId ? createActor(canisterId) : undefined;
